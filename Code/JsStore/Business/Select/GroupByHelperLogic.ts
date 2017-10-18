d02d4384bdbe21cf1357b611700a758dc7d04b09
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2017-10-18 10:31:15 +0530

    added code to do aggregate and group by for all type of aggregate functions

--- Code/JsStore/Business/Select/GroupByHelperLogic.ts
+++ Code/JsStore/Business/Select/GroupByHelperLogic.ts
@@ -6,116 +6,142 @@ module JsStore {
                     super();
                 }
 
-                private executeAggregateGroupBy = function (key = this.Query.GroupBy) {
-                    var Datas = this.Results,
+                private executeAggregateGroupBy = function () {
+                    var GrpQry = this.Query.GroupBy,
+                        IsGrpQryString = typeof GrpQry == 'string' ? true : false,
+                        Datas = this.Results,
                         lookupObject = {};
-                    var Results = [];
+                    //assign aggregate and free aggregate memory
+                    var AggregateQry = this.Query.Aggregate;
+                    this.Query.Aggregate = undefined;
                     //free results memory
                     this.Results = undefined;
-                    for (var i in Datas) {
-                        for (var prop in this.Query.Aggregate) {
+                    var Index, GrpColumn;
+                    var calculateAggregate = function () {
+                        for (var prop in AggregateQry) {
                             switch (prop) {
                                 case 'Count':
-                                    if (typeof this.Query.Aggregate[prop] == 'string') {
-                                        var Key = this.Query.Aggregate[prop],
-                                            Value = lookupObject[Datas[i][key]];
-                                        Value = Value ? Value["Count(" + Key + ")"] : 0;
-                                        Value += Datas[i][Key] ? 1 : 0;
-                                        Datas[i]["Count(" + Key + ")"] = Value;
+                                    var getCount = function (aggrColumn) {
+                                        var Value = lookupObject[Datas[Index][GrpColumn]];
+                                        //get old value
+                                        Value = Value ? Value["Count(" + aggrColumn + ")"] : 0;
+                                        //add with old value if data exist
+                                        Value += Datas[Index][aggrColumn] ? 1 : 0;
+                                        return Value;
                                     }
-                                    else if (Array.isArray(this.Query.Aggregate[prop])) {
-                                        for (var item in this.Query.Aggregate[prop]) {
-                                            var Key = this.Query.Aggregate[prop][item],
-                                                Value = lookupObject[Datas[i]["Count(" + Key + ")"]];
-                                            Value = Value ? Value : 0;
-                                            Value += Datas[i][Key] ? 1 : 0;
-                                            lookupObject[Datas[i]["Count(" + Key + ")"]] = Value;
+                                    if (typeof AggregateQry[prop] == 'string') {
+                                        var AggrColumn = AggregateQry[prop];
+                                        Datas[Index]["Count(" + AggrColumn + ")"] = getCount(AggrColumn);
+                                    }
+                                    else if (Array.isArray(AggregateQry[prop])) {
+                                        for (var item in AggregateQry[prop]) {
+                                            var AggrColumn = AggregateQry[prop][item];
+                                            Datas[Index]["Count(" + AggrColumn + ")"] = getCount(AggrColumn);
                                         }
                                     }
                                     break;
                                 case 'Max':
-                                    var getMax = function (key) {
-                                        var Result = 0;
-                                        for (var i in Datas) {
-                                            Result = Result > Datas[i][Key] ? Result : Datas[i][Key];
-                                        };
-                                        return Result;
+                                    var getMax = function (aggrColumn) {
+                                        var Value = lookupObject[Datas[Index][GrpColumn]];
+                                        //get old value
+                                        Value = Value ? Value["Max(" + aggrColumn + ")"] : 0;
+                                        //compare between old value and new value
+                                        return Value > Number(Datas[Index][aggrColumn]) ? Value : Datas[Index][aggrColumn];
                                     }
-                                    if (typeof this.Query.Aggregate[prop] == 'string') {
-                                        var Key = this.Query.Aggregate[prop];
-                                        Results["Max(" + Key + ")"] = getMax(Key);
+
+                                    if (typeof AggregateQry[prop] == 'string') {
+                                        var AggrColumn = AggregateQry[prop];
+                                        Datas[Index]["Max(" + AggrColumn + ")"] = getMax(AggrColumn);
                                     }
-                                    else if (Array.isArray(this.Query.Aggregate[prop])) {
-                                        for (var item in this.Query.Aggregate[prop]) {
-                                            var Key = this.Query.Aggregate[prop][key];
-                                            Results["Max(" + Key + ")"] = getMax(Key);
+                                    else if (Array.isArray(AggregateQry[prop])) {
+                                        for (var item in AggregateQry[prop]) {
+                                            var AggrColumn = AggregateQry[prop][item];
+                                            Datas[Index]["Max(" + AggrColumn + ")"] = getMax(AggrColumn);
                                         }
                                     }
                                     break;
                                 case 'Min':
-                                    var getMin = function (key) {
-                                        var Result = 0;
-                                        for (var i in Datas) {
-                                            Result = Result < Datas[i][Key] ? Result : Datas[i][Key];
-                                        };
-                                        return Result;
+                                    var getMin = function (aggrColumn) {
+                                        var Value = lookupObject[Datas[Index][GrpColumn]];
+                                        //get old value
+                                        Value = Value ? Value["Min(" + aggrColumn + ")"] : 0;
+                                        //compare between old value and new value
+                                        return Value < Number(Datas[Index][aggrColumn]) ? Value : Datas[Index][aggrColumn];
                                     }
-                                    if (typeof this.Query.Aggregate[prop] == 'string') {
-                                        var Key = this.Query.Aggregate[prop];
-                                        Results["Min(" + Key + ")"] = getMin(Key);
+
+                                    if (typeof AggregateQry[prop] == 'string') {
+                                        var AggrColumn = AggregateQry[prop];
+                                        Datas[Index]["Min(" + AggrColumn + ")"] = getMin(AggrColumn);
                                     }
-                                    else if (Array.isArray(this.Query.Aggregate[prop])) {
-                                        for (var item in this.Query.Aggregate[prop]) {
-                                            var Key = this.Query.Aggregate[prop][key];
-                                            Results["Min(" + Key + ")"] = getMin(Key);
+                                    else if (Array.isArray(AggregateQry[prop])) {
+                                        for (var item in AggregateQry[prop]) {
+                                            var AggrColumn = AggregateQry[prop][item];
+                                            Datas[Index]["Min(" + AggrColumn + ")"] = getMin(AggrColumn);
                                         }
                                     }
                                     break;
                                 case 'Sum':
-                                    var getSum = function (key) {
-                                        var Result = 0;
-                                        for (var i in Datas) {
-                                            Result += Datas[i][Key];
-                                        };
-                                        return Result;
+                                    var getSum = function (aggrColumn) {
+                                        var Value = lookupObject[Datas[Index][GrpColumn]];
+                                        //get old value
+                                        Value = Value ? Value["Sum(" + aggrColumn + ")"] : 0;
+                                        return Value + Number(Datas[Index][aggrColumn]);
                                     }
-                                    if (typeof this.Query.Aggregate[prop] == 'string') {
-                                        var Key = this.Query.Aggregate[prop];
-                                        Results["Sum(" + Key + ")"] = getSum(Key);
+                                    if (typeof AggregateQry[prop] == 'string') {
+                                        var AggrColumn = AggregateQry[prop];
+                                        Datas[Index]["Sum(" + AggrColumn + ")"] = getSum(AggrColumn);
                                     }
-                                    else if (Array.isArray(this.Query.Aggregate[prop])) {
-                                        for (var item in this.Query.Aggregate[prop]) {
-                                            var Key = this.Query.Aggregate[prop][key];
-                                            Results["Sum(" + Key + ")"] = getSum(Key);
+                                    else if (Array.isArray(AggregateQry[prop])) {
+                                        for (var item in AggregateQry[prop]) {
+                                            var AggrColumn = AggregateQry[prop][item];
+                                            Datas[Index]["Sum(" + AggrColumn + ")"] = getSum(AggrColumn);
                                         }
                                     }
                                     break;
                                 case 'Avg':
-                                    var getAvg = function (key) {
-                                        var Result = 0;
-                                        for (var i in Datas) {
-                                            Result += Datas[i][Key];
-                                        };
-                                        return Result / Datas.length;
+                                    var getAvg = function (aggrColumn) {
+                                        var Value = lookupObject[Datas[Index][GrpColumn]];
+                                        //get old value
+                                        Value = Value ? Value["Sum(" + aggrColumn + ")"] : 0;
+                                        return (Value + Number(Datas[Index][aggrColumn])) / Datas.length;
                                     }
-                                    if (typeof this.Query.Aggregate[prop] == 'string') {
-                                        var Key = this.Query.Aggregate[prop];
-                                        Results["Avg(" + Key + ")"] = getAvg(Key);
+
+                                    if (typeof AggregateQry[prop] == 'string') {
+                                        var AggrColumn = AggregateQry[prop];
+                                        Datas[Index]["Avg(" + AggrColumn + ")"] = getAvg(AggrColumn);
                                     }
-                                    else if (Array.isArray(this.Query.Aggregate[prop])) {
-                                        for (var item in this.Query.Aggregate[prop]) {
-                                            var Key = this.Query.Aggregate[prop][key];
-                                            Results["Avg(" + Key + ")"] = getAvg(Key);
+                                    else if (Array.isArray(AggregateQry[prop])) {
+                                        for (var item in AggregateQry[prop]) {
+                                            var AggrColumn = AggregateQry[prop][item];
+                                            Datas[Index]["Avg(" + AggrColumn + ")"] = getAvg(AggrColumn);
                                         }
                                     }
                                     break;
                             }
                         }
-                        lookupObject[Datas[i][key]] = Datas[i];
                     }
+
+                    if (IsGrpQryString) {
+                        GrpColumn = GrpQry;
+                        for (Index in Datas) {
+                            calculateAggregate();
+                            lookupObject[Datas[Index][GrpColumn]] = Datas[Index];
+                        }
+                    }
+                    else {
+                        for (Index in Datas) {
+                            for (var column in GrpQry) {
+                                GrpColumn = GrpQry[column];
+                                calculateAggregate();
+                                lookupObject[Datas[Index][GrpColumn]] = Datas[Index];
+                            }
+                        }
+
+                    }
+
                     //free datas memory
                     Datas = [];
-                    for (i in lookupObject) {
+                    for (var i in lookupObject) {
                         Datas.push(lookupObject[i]);
                     }
                     this.Results = Datas;
