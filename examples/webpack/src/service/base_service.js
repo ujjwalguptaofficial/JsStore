7151076f2f242ea7a928acf5df30cbf21466c6e1
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-05-01 11:41:11 +0530

    added jsstore v2 file in webpack example

--- examples/webpack/src/service/base_service.js
+++ examples/webpack/src/service/base_service.js
@@ -1,14 +1,25 @@
 import * as JsStore from "jsstore";
 export class BaseService {
- 
+
     constructor() {
         this.dbName = "Students";
         this.connection = new JsStore.Instance('fuck');
+        this.initDb();
     }
 
     initDb() {
         this.connection.isDbExist((exist) => {
-
+            if (exist) {
+                this.connection.openDb(this.dbName);
+            } else {
+                this.connection.createDb(this.dbName);
+            }
         });
+    }
+
+    getStudents() {
+        this.connection.select({
+            from: 'member',
+        })
     }
 }
