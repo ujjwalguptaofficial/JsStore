cd4ad8ec383666c6082109d45ed62d2b86c610a9
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-05-16 08:00:15 +0530

    v 2.0.5 release

--- src/output/jsstore.js
+++ src/output/jsstore.js
@@ -1,5 +1,5 @@
 /*!
- * @license :jsstore - V2.0.4 - 16/05/2018
+ * @license :jsstore - V2.0.5 - 16/05/2018
  * https://github.com/ujjwalguptaofficial/JsStore
  * Copyright (c) 2018 @Ujjwal Gupta; Licensed MIT
  */
