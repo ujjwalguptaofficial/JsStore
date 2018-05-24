9903018f8672932545b8e5498a369da2dfb726d4
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-05-24 10:56:00 +0530

    updated angular example

--- examples/angular/my-app/src/app/service/idb.service.ts
+++ examples/angular/my-app/src/app/service/idb.service.ts
@@ -1,5 +1,5 @@
 import * as JsStore from 'jsstore';
-import * as workerPath from 'file-loader?name=scripts/[name].[hash].js!jsstore/dist/jsstore.worker.js';
+import * as workerPath from 'file-loader?name=scripts/[name].[hash].js!jsstore/dist/jsstore.worker.min.js';
 export class IdbService {
   // this will make sure that we are using one instance
   // otherwise multiple instance will be created and thus multiple worker and that may create some problems
