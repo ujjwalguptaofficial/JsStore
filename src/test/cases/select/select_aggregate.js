b6b444c2d92d71ac5a5fa8b3729541bb86afde3d
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-07-29 23:30:42 +0530

    added more test case for aggregate

--- src/test/cases/select/select_aggregate.js
+++ src/test/cases/select/select_aggregate.js
@@ -74,4 +74,62 @@ describe('Test Aggregate option', function () {
         })
     });
 
+    it('select with agregate - min,max', function (done) {
+        Con.select({
+            from: 'Products',
+            aggregate: {
+                min: "Price",
+                max: "Price"
+            }
+        }).then(function (results) {
+            expect(results).to.be.an('array').length(77);
+            expect(results[0]).to.have.property('min(Price)').to.equal(2.5)
+            expect(results[0]).to.have.property('max(Price)').to.equal(263.5)
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    });
+
+    it('select with agregate - min,max,avg', function (done) {
+        Con.select({
+            from: 'Products',
+            aggregate: {
+                min: "Price",
+                max: "Price",
+                avg: "Price"
+            }
+        }).then(function (results) {
+            expect(results).to.be.an('array').length(77);
+            expect(results[0]).to.have.property('min(Price)').to.equal(2.5)
+            expect(results[0]).to.have.property('max(Price)').to.equal(263.5)
+            expect(results[0]).to.have.property('avg(Price)').to.equal(28.866363636363637)
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    });
+
+    it('select with agregate - min,max,avg,count', function (done) {
+        Con.select({
+            from: 'Products',
+            aggregate: {
+                min: "Price",
+                max: "Price",
+                avg: "Price",
+                count: ["Price","ProductName"]
+            }
+        }).then(function (results) {
+            expect(results).to.be.an('array').length(77);
+            expect(results[0]).to.have.property('min(Price)').to.equal(2.5)
+            expect(results[0]).to.have.property('max(Price)').to.equal(263.5)
+            expect(results[0]).to.have.property('avg(Price)').to.equal(28.866363636363637)
+            expect(results[0]).to.have.property('count(Price)').to.equal(77)
+            expect(results[0]).to.have.property('count(ProductName)').to.equal(77)
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    });
+
 });
