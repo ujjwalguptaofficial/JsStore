5a9af467b7e2891e1e174291646a6365a85ec322
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-08-02 09:18:38 +0530

    webpack config change for creating common module of jsstore web worker

--- src/webpack/webpack.dev.config.js
+++ src/webpack/webpack.dev.config.js
@@ -2,27 +2,26 @@ const path = require('path');
 const baseConfig = require('./webpack.base.config');
 const merge = require('webpack-merge');
 
-const libraryTarget = [{
-    type: "var",
-    name: 'jsstore.js'
-}, {
-    type: "commonjs2",
-    name: 'jsstore.commonjs2.js'
-}];
-
-function getConfigForTaget(target) {
-    return {
-        devtool: 'source-map',
-        output: {
-            path: path.join(__dirname, "./../output"),
-            filename: target.name,
-            library: 'JsStore',
-            libraryTarget: target.type
-        }
-    }
-}
 
 function createConfigsForAllLibraryTarget() {
+    const libraryTarget = [{
+        type: "var",
+        name: 'jsstore.js'
+    }, {
+        type: "commonjs2",
+        name: 'jsstore.commonjs2.js'
+    }];
+    const getConfigForTaget = function (target) {
+        return {
+            devtool: 'source-map',
+            output: {
+                path: path.join(__dirname, "./../output"),
+                filename: target.name,
+                library: 'JsStore',
+                libraryTarget: target.type
+            }
+        }
+    }
     var configs = [];
     libraryTarget.forEach(function (target) {
         configs.push(merge(baseConfig[0], getConfigForTaget(target)));
@@ -30,13 +29,35 @@ function createConfigsForAllLibraryTarget() {
     return configs;
 }
 
-module.exports = [...createConfigsForAllLibraryTarget(),
-    merge(baseConfig[1], {
-        devtool: 'source-map',
-        output: {
-            library: 'JsStoreWorker',
-            path: path.join(__dirname, "./../output"),
-            filename: "jsstore.worker.js"
+
+
+function createConfigsForAllLibraryTargetForWebWorker() {
+    const libraryTargetWorker = [{
+        type: "var",
+        name: 'jsstore.worker.js'
+    }, {
+        type: "commonjs2",
+        name: 'jsstore.worker.commonjs2.js'
+    }];
+
+    const getConfigForTaget = function (target) {
+        return {
+            devtool: 'source-map',
+            output: {
+                path: path.join(__dirname, "./../output"),
+                filename: target.name,
+                library: 'JsStoreWorker',
+                libraryTarget: target.type
+            }
         }
+    }
+    var configs = [];
+    libraryTargetWorker.forEach(function (target) {
+        configs.push(merge(baseConfig[1], getConfigForTaget(target)));
     })
+    return configs;
+}
+
+module.exports = [...createConfigsForAllLibraryTarget(),
+    ...createConfigsForAllLibraryTargetForWebWorker()
 ]
