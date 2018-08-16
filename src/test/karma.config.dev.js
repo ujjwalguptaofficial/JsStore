957c0087e2cf9ca040d804c6b67f8f009600e258
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-08-16 09:53:57 +0530

    increased timeout

--- src/test/karma.config.dev.js
+++ src/test/karma.config.dev.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
         frameworks: ['mocha', 'chai'],
         client: {
             mocha: {
-                timeout: 10000 // 6 seconds - upped from 2 seconds
+                timeout: 20000 // 6 seconds - upped from 2 seconds
             }
         },
         files: ['output/jsstore.js', 'output/jsstore.worker.js', ...files.list_of_files],
