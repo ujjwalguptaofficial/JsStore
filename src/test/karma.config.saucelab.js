9ad68335008a4c4c9c1f05ce6671815fc3845784
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-08-21 10:11:56 +0530

    added android browser

--- src/test/karma.config.saucelab.js
+++ src/test/karma.config.saucelab.js
@@ -23,6 +23,7 @@ module.exports = function (config) {
     createCuStomeLauncher('microsoftedge', 'Windows 10', 'latest', true);
     createCuStomeLauncher('Safari', 'macOS 10.12', 'latest', true);
     createCuStomeLauncher('iPhone 8 Simulator', 'iOS 11.2', null, true);
+    createCuStomeLauncher('Android Emulator', 'Android 6.0', null, true);
     createCuStomeLauncher('internet explorer', 'Windows 8.1', '11', true);
 
     config.set({
@@ -38,7 +39,7 @@ module.exports = function (config) {
             '/test/': '/base/test/',
             '/output/': '/base/output/'
         },
-        reporters: ['saucelabs'],
+        reporters: ['progress','saucelabs'],
         port: 9876, // karma web server port
         colors: true,
         logLevel: config.LOG_INFO,
