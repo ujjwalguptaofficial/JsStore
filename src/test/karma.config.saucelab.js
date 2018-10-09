8ef8a41782eb333e5c0980b8b983899a61a7758b
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-10-09 08:39:06 +0530

    turned on all sauce lab test

--- src/test/karma.config.saucelab.js
+++ src/test/karma.config.saucelab.js
@@ -15,14 +15,14 @@ module.exports = function (config) {
             extendedDebugging: debug
         }
     }
-    // createCuStomeLauncher('firefox', 'macOS 10.13', 'latest', true);
-    // createCuStomeLauncher('firefox', 'Windows 10', '61.0', true);
-    // createCuStomeLauncher('chrome', 'linux', 'latest', true);
-    // createCuStomeLauncher('chrome', 'macOS 10.13', 'latest', true);
-    // createCuStomeLauncher('chrome', 'Windows 10', 'latest', true);
-    // createCuStomeLauncher('microsoftedge', 'Windows 10', 'latest', true);
-    createCuStomeLauncher('internet explorer', 'Windows 8.1', '11', true);
-    // createCuStomeLauncher('Safari', 'macOS 10.13', 'latest', true);
+    createCuStomeLauncher('firefox', 'macOS 10.13', 'latest', true);
+    createCuStomeLauncher('firefox', 'Windows 10', '61.0', true);
+    createCuStomeLauncher('chrome', 'linux', 'latest', true);
+    createCuStomeLauncher('chrome', 'macOS 10.13', 'latest', true);
+    createCuStomeLauncher('chrome', 'Windows 10', 'latest', true);
+    createCuStomeLauncher('microsoftedge', 'Windows 10', 'latest', true);
+    //createCuStomeLauncher('internet explorer', 'Windows 8.1', '11', true);
+    createCuStomeLauncher('Safari', 'macOS 10.13', 'latest', true);
 
 
     config.set({
