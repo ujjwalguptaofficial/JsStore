8dd936fa6699493864ed9393672bd14490d1daa6
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-10-28 10:48:42 +0530

    changed edge version

--- src/test/karma.config.saucelab.js
+++ src/test/karma.config.saucelab.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
     createCustomLauncher('chrome', 'linux', 'latest', true);
     createCustomLauncher('chrome', 'macOS 10.13', 'latest', true);
     createCustomLauncher('chrome', 'Windows 10', '69.0.3497', true);
-    createCustomLauncher('microsoftedge', 'Windows 10', '17.17134.0', true);
+    createCustomLauncher('microsoftedge', 'Windows 10', '17', true);
     //createCuStomeLauncher('internet explorer', 'Windows 8.1', '11', true);
     createCustomLauncher('Safari', 'macOS 10.13', 'latest', true);
 
