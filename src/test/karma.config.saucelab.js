dcd27de95244b51fdc88330c0ac7e4739f257333
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-10-31 10:20:17 +0530

    turned off search for safari macos

--- src/test/karma.config.saucelab.js
+++ src/test/karma.config.saucelab.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
    
    //createCustomLauncher('microsoftedge', 'Windows 10', '17', true);
     //createCuStomeLauncher('internet explorer', 'Windows 8.1', '11', true);
-    createCustomLauncher('Safari', 'macOS 10.13', 'latest', true);
+    //createCustomLauncher('Safari', 'macOS 10.13', 'latest', true);
 
 
     config.set({
