eaf9360d67fb88d9ba7f9c0c05a9da817e96d45b
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2018-11-30 07:52:11 +0530

    chnaged the os to wndows7

--- src/test/karma/karma.config.saucelab.js
+++ src/test/karma/karma.config.saucelab.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         }
     }
 
-    createCustomLauncher('chrome', 'Windows 10', 'latest', true);
+    createCustomLauncher('chrome', 'Windows 7', 'latest', true);
     // createCustomLauncher('firefox', 'macOS 10.13', 'latest', true);
     // createCustomLauncher('firefox', 'Windows 10', '61.0', true);
     // createCustomLauncher('chrome', 'linux', 'latest', true);
