f9cef1bab472dc393fb0326895483f45176903a8
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-22 11:56:07 +0530

    updated sauce lab browser

--- test/karma/karma.config.saucelab.js
+++ test/karma/karma.config.saucelab.js
@@ -27,21 +27,20 @@ var createCustomLauncher = function (browser, platform, version, debug) {
 }
 
 if (program.batch1) {
-    createCustomLauncher('firefox', 'macOS 10.13', 'latest', true);
+    createCustomLauncher('firefox', 'macOS 10.14', 'latest', true);
     createCustomLauncher('firefox', 'Windows 10', '61.0', true);
-    // createCustomLauncher('chrome', 'linux', 'latest', true);
-    createCustomLauncher('chrome', 'macOS 10.13', 'latest', true);
+    createCustomLauncher('chrome', 'macOS 10.14', 'latest', true);
 
 } else if (program.batch2) {
-    createCustomLauncher('Safari', 'macOS 10.13', 'latest', true);
+    createCustomLauncher('Safari', 'macOS 10.14', 'latest', true);
     createCustomLauncher('chrome', 'Windows 10', 'latest', true);
     createCustomLauncher('microsoftedge', 'Windows 10', 'latest', true);
     createCustomLauncher('internet explorer', 'Windows 10', 'latest', true);
 }
-else {
-    // createCustomLauncher('Safari', 'iOS', 'latest', true);
-    // createCustomLauncher('chrome', 'android', 'latest', true);
-}
+// else {
+//     // createCustomLauncher('Safari', 'iOS', 'latest', true);
+//     // createCustomLauncher('chrome', 'android', 'latest', true);
+// }
 
 module.exports = function (config) {
 
