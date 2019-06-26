323ee4b9578ce7d7f0756f19cdb0aa21520eee68
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-26 07:50:29 +0530

    modified ie_builder script to generate both dev & min file

---
+++ bin/polyfill.all.js
@@ -0,0 +1,92 @@
+!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?n():"function"==typeof define&&define.amd?define(n):n()}(0,function(){"use strict";function e(e){var n=this.constructor;return this.then(function(t){return n.resolve(e()).then(function(){return t})},function(t){return n.resolve(e()).then(function(){return n.reject(t)})})}function n(){}function t(e){if(!(this instanceof t))throw new TypeError("Promises must be constructed via new");if("function"!=typeof e)throw new TypeError("not a function");this._state=0,this._handled=!1,this._value=undefined,this._deferreds=[],u(e,this)}function o(e,n){for(;3===e._state;)e=e._value;0!==e._state?(e._handled=!0,t._immediateFn(function(){var t=1===e._state?n.onFulfilled:n.onRejected;if(null!==t){var o;try{o=t(e._value)}catch(f){return void i(n.promise,f)}r(n.promise,o)}else(1===e._state?r:i)(n.promise,e._value)})):e._deferreds.push(n)}function r(e,n){try{if(n===e)throw new TypeError("A promise cannot be resolved with itself.");if(n&&("object"==typeof n||"function"==typeof n)){var o=n.then;if(n instanceof t)return e._state=3,e._value=n,void f(e);if("function"==typeof o)return void u(function(e,n){return function(){e.apply(n,arguments)}}(o,n),e)}e._state=1,e._value=n,f(e)}catch(r){i(e,r)}}function i(e,n){e._state=2,e._value=n,f(e)}function f(e){2===e._state&&0===e._deferreds.length&&t._immediateFn(function(){e._handled||t._unhandledRejectionFn(e._value)});for(var n=0,r=e._deferreds.length;r>n;n++)o(e,e._deferreds[n]);e._deferreds=null}function u(e,n){var t=!1;try{e(function(e){t||(t=!0,r(n,e))},function(e){t||(t=!0,i(n,e))})}catch(o){if(t)return;t=!0,i(n,o)}}var c=setTimeout;t.prototype["catch"]=function(e){return this.then(null,e)},t.prototype.then=function(e,t){var r=new this.constructor(n);return o(this,new function(e,n,t){this.onFulfilled="function"==typeof e?e:null,this.onRejected="function"==typeof n?n:null,this.promise=t}(e,t,r)),r},t.prototype["finally"]=e,t.all=function(e){return new t(function(n,t){function o(e,f){try{if(f&&("object"==typeof f||"function"==typeof f)){var u=f.then;if("function"==typeof u)return void u.call(f,function(n){o(e,n)},t)}r[e]=f,0==--i&&n(r)}catch(c){t(c)}}if(!e||"undefined"==typeof e.length)throw new TypeError("Promise.all accepts an array");var r=Array.prototype.slice.call(e);if(0===r.length)return n([]);for(var i=r.length,f=0;r.length>f;f++)o(f,r[f])})},t.resolve=function(e){return e&&"object"==typeof e&&e.constructor===t?e:new t(function(n){n(e)})},t.reject=function(e){return new t(function(n,t){t(e)})},t.race=function(e){return new t(function(n,t){for(var o=0,r=e.length;r>o;o++)e[o].then(n,t)})},t._immediateFn="function"==typeof setImmediate&&function(e){setImmediate(e)}||function(e){c(e,0)},t._unhandledRejectionFn=function(e){void 0!==console&&console&&console.warn("Possible Unhandled Promise Rejection:",e)};var l=function(){if("undefined"!=typeof self)return self;if("undefined"!=typeof window)return window;if("undefined"!=typeof global)return global;throw Error("unable to locate global object")}();"Promise"in l?l.Promise.prototype["finally"]||(l.Promise.prototype["finally"]=e):l.Promise=t});
+
+"use strict";
+
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function value(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this); // 2. Let len be ? ToLength(? Get(O, "length")).
+
+      var len = o.length >>> 0; // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      } // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+
+
+      var thisArg = arguments[1]; // 5. Let k be 0.
+
+      var k = 0; // 6. Repeat, while k < len
+
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        var kValue = o[k];
+
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        } // e. Increase k by 1.
+
+
+        k++;
+      } // 7. Return -1.
+
+
+      return -1;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+"use strict";
+
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function value(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this); // 2. Let len be ? ToLength(? Get(O, "length")).
+
+      var len = o.length >>> 0; // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      } // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+
+
+      var thisArg = arguments[1]; // 5. Let k be 0.
+
+      var k = 0; // 6. Repeat, while k < len
+
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        } // e. Increase k by 1.
+
+
+        k++;
+      } // 7. Return undefined.
+
+
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
--- src/ie_build_helper.js
+++ src/ie_build_helper.js
@@ -2,40 +2,65 @@ var FILE_ENCODING = 'utf-8',
 
     EOL = '\n';
 
-var fs = require('fs');
+var { ensureDir, readFileSync, writeFileSync } = require('fs-extra');
+
+ensureDir('bin')
 
 function concat(opts) {
 
     var fileList = opts.src;
     var distPath = opts.dest;
     var out = fileList.map(function (filePath) {
-        return fs.readFileSync(filePath, FILE_ENCODING);
+        return readFileSync(filePath, FILE_ENCODING);
     });
 
-    fs.writeFileSync(distPath, out.join(EOL), FILE_ENCODING);
+    writeFileSync(distPath, out.join(EOL), FILE_ENCODING);
     console.log(' ' + distPath + ' built.');
 }
 
 function uglify(srcPath, distPath) {
-    var
-        uglyfyJS = require('uglify-js');
+    var uglyfyJS = require('uglify-js');
     //     jsp = uglyfyJS.parser,
     //     pro = uglyfyJS.uglify,
     //     ast = jsp.parse(_fs.readFileSync(srcPath, FILE_ENCODING));
 
     // ast = pro.ast_mangle(ast);
     // ast = pro.ast_squeeze(ast);
-    var output = uglyfyJS.minify(fs.readFileSync(srcPath, FILE_ENCODING));
-    fs.writeFileSync(distPath, output.code, FILE_ENCODING);
+    var output = uglyfyJS.minify(readFileSync(srcPath, {
+        encoding: FILE_ENCODING
+    }));
+    if (output.error != null) {
+        throw output.error;
+    }
+    writeFileSync(distPath, output.code, {
+        encoding: FILE_ENCODING
+    });
     console.log(' ' + distPath + ' built.');
 }
 
 //concate files
+
 concat({
-    src: ['build/jsstore.worker.min.js',
+    src: [
         'node_modules/promise-polyfill/dist/polyfill.min.js',
         'src/ie_polyfill/find_index.js',
         'src/ie_polyfill/find.js'
+    ],
+    dest: 'bin/polyfill.all.js'
+})
+
+uglify('bin/polyfill.all.js', 'bin/polyfill.all.min.js')
+
+concat({
+    src: ['build/jsstore.worker.js',
+        'bin/polyfill.all.min.js'
+    ],
+    dest: 'build/jsstore.worker.ie.js'
+})
+
+concat({
+    src: ['build/jsstore.worker.min.js',
+        'bin/polyfill.all.min.js'
     ],
     dest: 'build/jsstore.worker.ie.min.js'
 })
