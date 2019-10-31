1b4e23c369694d3647a3fdfb2ca5ebe40dd99453
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-10-31 10:06:43 +0530

    es6 code to ie11 compatible

--- test/cases/delete/delete_complex.js
+++ test/cases/delete/delete_complex.js
@@ -1,14 +1,14 @@
 describe('Test remove complex case', function () {
     it('remove with multiple or', function (done) {
         var where_query = {
-                country: 'Mexico',
-                or: {
-                    city: 'Madrid',
-                    address: {
-                        like: '%a%'
-                    }
+            country: 'Mexico',
+            or: {
+                city: 'Madrid',
+                address: {
+                    like: '%a%'
                 }
-            },
+            }
+        },
             count;
         con.select({
             from: 'Customers',
@@ -32,17 +32,17 @@ describe('Test remove complex case', function () {
 
     it("sql - SELECT * FROM Customers WHERE country='Mexico' and (city='London' or address like '%a%')", function (done) {
         var where_query = [{
-                    country: 'Mexico'
-                },
-                {
-                    city: 'London',
-                    or: {
-                        address: {
-                            like: '%a%'
-                        }
-                    }
+            country: 'Mexico'
+        },
+        {
+            city: 'London',
+            or: {
+                address: {
+                    like: '%a%'
                 }
-            ],
+            }
+        }
+        ],
             count;
         con.select({
             from: 'Customers',
@@ -66,17 +66,17 @@ describe('Test remove complex case', function () {
 
     it("sql - SELECT * FROM Customers WHERE country='Mexico' or (city='London' and address like '%a%')", function (done) {
         var where_query = [{
-                    country: 'Mexico'
-                },
-                {
-                    or: {
-                        city: 'London',
-                        address: {
-                            like: '%a%'
-                        }
-                    }
+            country: 'Mexico'
+        },
+        {
+            or: {
+                city: 'London',
+                address: {
+                    like: '%a%'
                 }
-            ],
+            }
+        }
+        ],
             count;
         con.select({
             from: 'Customers',
@@ -100,16 +100,13 @@ describe('Test remove complex case', function () {
 
 
     it("sql - DELETE FROM Customers WHERE country regex /Mexico|Brazil/", function (done) {
-        var from  = 'Customers'
-          , where = { country: { regex: /Mexico|Brazil/ } }
-          , count ;
+        var from = 'Customers'
+            , where = { country: { regex: /Mexico|Brazil/ } }
+            , count;
 
-        con.select({from, where})
-           .then(function (results) {
+        con.select({ from: from, where: where }).then(function (results) {
             count = results.length;
-
-            con.remove({from, where })
-            .then(function (results) {
+            con.remove({ from: from, where: where }).then(function (results) {
                 expect(results).to.be.an('number').to.equal(count);
                 done();
             }).catch(function (err) {
