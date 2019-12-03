72925f1de8b6b3512ae72946b1dc7fe34528352e
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-12-03 22:01:21 +0530

    rm unused code

--- src/worker/business/union/index.ts
+++ src/worker/business/union/index.ts
@@ -1,14 +1,11 @@
-import { Base } from "../base";
 import { SelectQuery } from "../../types";
-import { promiseAll } from "../../helpers/index";
 import * as Select from '../select/index';
 import { IError } from "../../interfaces";
 
-export class Union extends Base {
+export class Union {
     execute(query: SelectQuery[], onSuccess: (results: object[]) => void, onError: (err: IError) => void) {
         let index = 0;
         const hashMap = {};
-        const columns = this.getTable(query[0].from).columns;
         const fetchData = () => {
             if (index < query.length) {
                 new Select.Instance(query[index++], (selectResult) => {
