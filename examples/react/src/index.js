a20d5c47d3c19eaabba27fd3207aa313be57356c
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-12-16 20:19:57 +0530

    remove react example

--- examples/react/src/index.js
+++
@@ -1,5 +0,0 @@
-import React  from "react";
-import ReactDOM from "react-dom";
-import Layout from "./component/layout";
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>,app);
