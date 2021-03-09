21790ef09329960bcf8e22169c07ce61a8434af8
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-03-09 10:53:00 +0530

    update karma config to use base config

--- test/karma/karma.config.saucelab.js
+++ test/karma/karma.config.saucelab.js
@@ -1,5 +1,7 @@
 var program = require('commander');
 var files = require('../files');
+var base = require('./karma.config.base');
+
 
 program.option('-b1, --batch1', 'start the batch1').
     option('-b2, --batch2', 'start the batch2').
@@ -49,56 +51,48 @@ module.exports = function (config) {
         process.exit(1)
     }
     config.set({
-        basePath: '../../',
-        frameworks: ['mocha', 'chai'],
-        client: {
-            mocha: {
-                timeout: 200000
-            }
-        },
-        browserDisconnectTimeout: 60000,
-        files: ['test/karma/sauce.global.js', 'build/jsstore.min.js', 'build/jsstore.worker.min.js', ...files.list_of_files],
-        proxies: {
-            '/test/': '/base/test/',
-            '/output/': '/base/build/'
-        },
-        reporters: ['mocha', 'saucelabs', 'html'],
-        port: 9876, // karma web server port
-        colors: true,
-        logLevel: config.LOG_INFO,
-        captureConsole: true,
-        browserConsoleLogOptions: {
-            terminal: false
-        },
-        sauceLabs: {
-            testName: 'jsstore sauce lab test',
-            recordScreenshots: false,
-            connectOptions: {
-                //port: 5757,
-                logfile: 'sauce_connect.log'
+        ...base(config), ...{
+            files: [
+                'test/karma/sauce.global.js',
+                'build/jsstore.min.js',
+                'build/jsstore.worker.min.js',
+                ...files.list_of_files
+            ],
+            reporters: ['mocha', 'saucelabs', 'html'],
+            captureConsole: true,
+            browserConsoleLogOptions: {
+                terminal: false
             },
-            // connectOptions: { // for safair and ie
-            //     noSslBumpDomains: "all"
-            // },
-            public: 'public'
-        },
-        // Increase timeout in case connection in CI is slow
-        captureTimeout: 240000, //120000,
-        browserNoActivityTimeout: 240000,
-        customLaunchers: customLaunchers,
-        //browserDisconnectTolerance: 1,
-        browsers: Object.keys(customLaunchers),
-        concurrency: 5,
-        singleRun: true,
-        htmlReporter: {
-            outputFile: 'report/units.html',
+            sauceLabs: {
+                testName: 'jsstore sauce lab test',
+                recordScreenshots: false,
+                connectOptions: {
+                    //port: 5757,
+                    logfile: 'sauce_connect.log'
+                },
+                // connectOptions: { // for safair and ie
+                //     noSslBumpDomains: "all"
+                // },
+                public: 'public'
+            },
+            // Increase timeout in case connection in CI is slow
+            captureTimeout: 240000, //120000,
+            browserNoActivityTimeout: 240000,
+            customLaunchers: customLaunchers,
+            //browserDisconnectTolerance: 1,
+            browsers: Object.keys(customLaunchers),
+            concurrency: 5,
+            singleRun: true,
+            htmlReporter: {
+                outputFile: 'report/units.html',
 
-            // Optional
-            pageTitle: 'unit Tests',
-            subPageTitle: 'A sample project description',
-            groupSuites: true,
-            useCompactStyle: true,
-            useLegacyStyle: true
+                // Optional
+                pageTitle: 'unit Tests',
+                subPageTitle: 'A sample project description',
+                groupSuites: true,
+                useCompactStyle: true,
+                useLegacyStyle: true
+            }
         }
-    })
+    });
 }
