7a590fc27e02bf043f2f1986b417602334071a30
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-14 23:23:10 +0530

    create seperate config for firefox

--- test/karma/karma.config.firefox.js
+++ test/karma/karma.config.firefox.js
@@ -1,13 +1,13 @@
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 var files = require('../files');
 var base = require('./karma.config.base');
-
 module.exports = function (config) {
     config.set({
         ...base(config), ...{
-            files: ['test/karma/prod.global.js', 'build/jsstore.min.js', 'build/jsstore.worker.min.js',
-                'build/jsstore.worker.ie.min.js', ...files.list_of_files
+            browsers: ['HeadlessFireFox'],
+            files: ['build/jsstore.js', 'build/jsstore.worker.js',
+                ...files.list_of_files
             ],
         }
-    });
+    })
 }
