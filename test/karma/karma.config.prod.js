5e1c960a48b9ab42d44d6f67f4a5120ec578f942
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-14 23:20:28 +0530

    add firefox launcher

--- test/karma/karma.config.prod.js
+++ test/karma/karma.config.prod.js
@@ -5,6 +5,7 @@ var base = require('./karma.config.base');
 module.exports = function (config) {
     config.set({
         ...base(config), ...{
+            browsers: ['HeadlessChrome', 'HeadlessFireFox'],
             files: ['test/karma/prod.global.js', 'build/jsstore.min.js', 'build/jsstore.worker.min.js',
                 'build/jsstore.worker.ie.min.js', ...files.list_of_files
             ],
