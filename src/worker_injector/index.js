8e0c30eff37550762ed00c098f4a861a5f716845
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-25 16:41:14 +0530

    fix path issue with workerInjector

--- src/worker_injector/index.js
+++ src/worker_injector/index.js
@@ -2,10 +2,10 @@ module.exports = {
     setup(connection, param) {
         connection['$worker'] = (() => {
             if (process.env.NODE_ENV === 'production') {
-                return require('./jsstore.worker.commonjs2.min.js');
+                return require('../jsstore.worker.commonjs2.min.js');
             }
             else {
-                module.exports = require('./jsstore.worker.commonjs2.js');
+                return require('../jsstore.worker.commonjs2.js');
             }
         })();
         connection.initQueryManager_();
